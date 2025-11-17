1e10089aa2c5ee549d36e55938d58ebbd19010d6
MAHIPAL SINGH <manish7.2.2004@gmail.com>
2025-11-17 22:57:07 +0530

    playing with get post and delete

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("helloooooo");
 });
+app.post("/user", (req, res) => {
+  res.send("user updated in db ");
+});
+app.delete("/user", (req, res) => {
+  res.send("user deleted successfully");
+});
 
 app.listen(3000, () => {
   console.log("listening at port no 3000");
