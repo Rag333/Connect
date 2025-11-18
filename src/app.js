adaa1e161c4635a9aeed617452cdafc5bb500258
MAHIPAL SINGH <manish7.2.2004@gmail.com>
2025-11-18 13:10:30 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("helloooooo");
-});
-app.post("/user", (req, res) => {
-  res.send("user updated in db ");
-});
-app.delete("/user", (req, res) => {
-  res.send("user deleted successfully");
-});
-
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1 response");
+    // res.send("1 response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2 response");
+    //  res.send("2 response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("3 response");
+    res.send("3 response");
+    //  next();
+  }
+);
 app.listen(3000, () => {
   console.log("listening at port no 3000");
 });
